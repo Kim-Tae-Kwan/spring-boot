80df4bc5767664fb4dab55a15b56b5c18c7716cc
kim <vodjaos@gmail.com>
2021-12-31 16:40:19 +0900

    [MOD] application  시작 시점에, 초기 데이터 삽입을 위한 CommadLineRunner 추가.

--- src/main/java/com/study/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/study/springsecurity/SpringSecurityApplication.java
@@ -1,7 +1,16 @@
 package com.study.springsecurity;
 
+import com.study.springsecurity.model.Member;
+import com.study.springsecurity.model.Role;
+import com.study.springsecurity.service.MemberService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -10,4 +19,21 @@ public class SpringSecurityApplication {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run(MemberService memberService){
+        return args -> {
+            memberService.saveRole(new Role(null, "USER"));
+            memberService.saveRole(new Role(null, "ADMIN"));
+            memberService.saveRole(new Role(null, "MASTER"));
+
+            memberService.saveMember(new Member(null, "kim", "tkk@vazilcompany.com", "1234", new ArrayList<>()));
+            memberService.saveMember(new Member(null, "seo", "seo@vazilcompany.com", "1234", new ArrayList<>()));
+            memberService.saveMember(new Member(null, "ghp", "ghp@vazilcompany.com", "1234", new ArrayList<>()));
+
+            memberService.addRoleToMember("tkk@vazilcompany.com", "USER");
+            memberService.addRoleToMember("seo@vazilcompany.com", "ADMIN");
+            memberService.addRoleToMember("ghp@vazilcompany.com", "MASTER");
+
+        };
+    }
 }
