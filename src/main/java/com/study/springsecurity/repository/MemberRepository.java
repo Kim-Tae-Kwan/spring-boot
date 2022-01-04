e4390d9fc41c2c0809b7469307eb038dec7f07ae
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-01-04 22:35:02 +0900

    [MOD] Service interface 삭제.

--- src/main/java/com/study/springsecurity/repository/MemberRepository.java
+++ src/main/java/com/study/springsecurity/repository/MemberRepository.java
@@ -1,9 +1,8 @@
 package com.study.springsecurity.repository;
 
-import com.study.springsecurity.model.Member;
+import com.study.springsecurity.model.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
     Member findByEmail(String email);
 }
--- src/main/java/com/study/springsecurity/service/MemberService.java
+++ src/main/java/com/study/springsecurity/service/MemberService.java
@@ -1,14 +1,58 @@
 package com.study.springsecurity.service;
 
-import com.study.springsecurity.model.Member;
-import com.study.springsecurity.model.Role;
+import com.study.springsecurity.model.entity.Member;
+import com.study.springsecurity.model.entity.Role;
+import com.study.springsecurity.repository.MemberRepository;
+import com.study.springsecurity.repository.RoleRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
-public interface MemberService {
-    Member saveMember(Member member);
-    Role saveRole(Role role) ;
-    void addRoleToMember(String email, String roleName);
-    Member getMember(String email);
-    List<Member> getMembers();
+@Log4j2
+@Transactional
+@AllArgsConstructor
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Member saveMember(Member member) {
+        log.info("Saving new {} to the database", member.getName());
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+        return memberRepository.save(member);
+    }
+
+    public Role saveRole(Role role) {
+        log.info("Saving new {} to the database", role.getName());
+        return roleRepository.save(role);
+    }
+
+    public void addRoleToMember(String email, String roleName) {
+        log.info("Adding role {} to email {}", roleName, email);
+        Member member = memberRepository.findByEmail(email);
+        Role role = roleRepository.findByName(roleName);
+        member.getRoles().add(role);
+    }
+
+    public Member getMember(String email) {
+        log.info("Fetching member {}", email);
+        return memberRepository.findByEmail(email);
+    }
+
+    public List<Member> getMembers() {
+        log.info("Fetching members");
+        return memberRepository.findAll();
+    }
 }
