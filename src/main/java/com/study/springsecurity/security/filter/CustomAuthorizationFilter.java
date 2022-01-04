a0755c4727cb74bcf094c2b42f0fb04669bec239
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-01-04 22:39:22 +0900

    [ADD] 인가 역할의 Authorization Filter 추가.

---
+++ src/main/java/com/study/springsecurity/security/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,59 @@
+package com.study.springsecurity.security.filter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static java.util.Arrays.stream;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+
+@Log4j2
+public class CustomAuthorizationFilter extends OncePerRequestFilter { //application에 들어오는 모든 Request를 필터링 함.
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getServletPath().equals("/api/login")){ //Request path가 로그인이면 다음 Filter 실행.
+            filterChain.doFilter(request, response);
+        }else{// Authorization 수행.
+            String authorizationHeader = request.getHeader("Authorization");
+            if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                try {
+                    String token = authorizationHeader.substring("Bearer ".length());
+                    Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                    JWTVerifier verifier = JWT.require(algorithm).build();
+                    DecodedJWT decodedJWT = verifier.verify(token);
+                    String username = decodedJWT.getSubject();
+                    String[] roles = decodedJWT.getClaim("roles").asArray(String.class);
+                    Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+                    stream(roles).forEach(role -> {
+                        authorities.add(new SimpleGrantedAuthority(role));
+                    });
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    filterChain.doFilter(request, response);
+
+                } catch (Exception exception) {
+                    log.error("Authorization Error : {}", exception.getMessage());
+                    response.setHeader("error", exception.getMessage());
+                    response.sendError(FORBIDDEN.value());
+                }
+            }else {
+                filterChain.doFilter(request, response);
+            }
+        }
+    }
+}
