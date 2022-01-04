b6a901d9ce537bf5a47cf8dee26ecf53e888d204
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-01-04 22:40:04 +0900

    [MOD] UserDetailsService 분리.

---
+++ src/main/java/com/study/springsecurity/security/SecurityUserDetailService.java
@@ -0,0 +1,34 @@
+package com.study.springsecurity.security;
+
+import com.study.springsecurity.model.entity.Member;
+import com.study.springsecurity.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+@Service
+public class SecurityUserDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+        if(member == null){
+            throw  new UsernameNotFoundException("User not found");
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        member.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+
+        return new User(member.getEmail(), member.getPassword(), authorities);
+    }
+}
