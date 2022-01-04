f1ad97f867c9aa3092e1b62cdcf408b49e490857
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-01-04 22:41:59 +0900

    [MOD] 토큰 재발급 로직을 위해 Mapping 메소드 추가.

--- src/main/java/com/study/springsecurity/controller/MemberController.java
+++ src/main/java/com/study/springsecurity/controller/MemberController.java
@@ -1,19 +1,32 @@
 package com.study.springsecurity.controller;
 
-import com.study.springsecurity.model.Member;
-import com.study.springsecurity.model.Role;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.springsecurity.model.dto.roleAddRequestDto;
+import com.study.springsecurity.model.entity.Member;
+import com.study.springsecurity.model.entity.Role;
 import com.study.springsecurity.service.MemberService;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Log4j2
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @RestController
@@ -50,10 +63,42 @@ public class MemberController {
         memberService.addRoleToMember(requestDto.getEmail(), requestDto.getRoleName());
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/token/refresh")
+    public void getRefresh(HttpServletRequest request, HttpServletResponse response){
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refresh_token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refresh_token);
+                String username = decodedJWT.getSubject();
+                Member member = memberService.getMember(username);
+                String access_token = JWT.create()
+                        .withSubject(member.getEmail())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles", member.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
+                        .sign(algorithm);
+
+                Map<String,String> tokens = new HashMap<>();
+                tokens.put("access_token", access_token);
+                tokens.put("refresh_token", refresh_token);
+                response.setContentType(APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+            } catch (Exception exception) {
+                log.error("Authorization Error : {}", exception.getMessage());
+                response.setHeader("error", exception.getMessage());
+//                Map<String, String> error = new HashMap<>();
+//                error.put("error_message", exception.getMessage());
+//                response.setContentType(APPLICATION_JSON_VALUE);
+//                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        }else {
+            throw new RuntimeException("refresh token is missing.");
+        }
+    }
 }
 
-@Data
-class roleAddRequestDto{
-    private String email;
-    private String roleName;
-}
+
