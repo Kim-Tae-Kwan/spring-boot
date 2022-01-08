56d86b1d3cbefe698ea14f57c5c4a14ecad9a45e
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-01-09 00:23:57 +0900

    [MOD] swagger 오류 해결.

--- security/src/main/java/com/study/springsecurity/swagger/SwaggerConfig.java
+++ security/src/main/java/com/study/springsecurity/swagger/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     @Bean
     public Docket swaggerApi() {
         return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.study.security"))
+                .apis(RequestHandlerSelectors.basePackage("com.study.springsecurity"))// package명과 같아야함!.
                 .paths(PathSelectors.any())
                 .build()
                 .useDefaultResponseMessages(false); // 기본으로 세팅되는 200,401,403,404 메시지를 표시 하지 않음
