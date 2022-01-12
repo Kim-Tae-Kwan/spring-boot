4f4a9a85a3a9cb6304debee60b86463da166fe3f
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-01-12 09:03:46 +0900

    [MOD] Mapping 메소드 위치 변경.

--- file-server/src/main/java/com/study/fileserver/controller/FileController.java
+++ file-server/src/main/java/com/study/fileserver/controller/FileController.java
@@ -5,20 +5,12 @@ import com.study.fileserver.service.FileService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Log4j2
 @AllArgsConstructor
@@ -26,16 +18,15 @@ import java.util.stream.Collectors;
 public class FileController {
     private final FileService fileService;
 
+    @GetMapping("/downloadFile/{fileName}")
+    public ResponseEntity<Resource> download(@PathVariable String fileName, HttpServletRequest request) {
+        return fileService.loadFile(fileName);
+    }
+
     @PostMapping("/upload")
     public ResponseEntity<List<UploadFileResponse>> upload(@RequestParam("files") MultipartFile[] files) {
         List<UploadFileResponse> responses = fileService.saveFiles(files);
         return ResponseEntity.ok()
                 .body(responses);
     }
-
-    @GetMapping("/downloadFile/{fileName}")
-    public ResponseEntity<Resource> download(@PathVariable String fileName, HttpServletRequest request) {
-        return fileService.loadFile(fileName);
-    }
-
 }
