7883273dafbec910cccc2245ac71a5f7255a7e3f
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-01-13 13:37:39 +0900

    [MOD] API path prefix 추가.

--- file-server/src/main/java/com/study/fileserver/controller/FileController.java
+++ file-server/src/main/java/com/study/fileserver/controller/FileController.java
@@ -3,17 +3,15 @@ package com.study.fileserver.controller;
 import com.study.fileserver.model.UploadFileResponse;
 import com.study.fileserver.service.FileService;
 import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-@Log4j2
+
 @AllArgsConstructor
+@RequestMapping("/file")
 @RestController
 public class FileController {
     private final FileService fileService;
