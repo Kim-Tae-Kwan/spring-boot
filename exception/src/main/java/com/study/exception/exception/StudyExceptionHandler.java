d9ce557f168c482a4fa0dbe94ed465b683de8369
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-01-24 22:02:18 +0900

    [MOD] ExceptionAdvice에 ExceptionCode, ExceptionResponse 적용.

--- exception/src/main/java/com/study/exception/exception/StudyExceptionHandler.java
+++ exception/src/main/java/com/study/exception/exception/StudyExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.study.exception.exception;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestControllerAdvice
 public class StudyExceptionHandler {
     @ExceptionHandler(value = {TestException.class})
-    public ResponseEntity<Object> testException(TestException exception){
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<Object> testException(TestException exception, HttpServletRequest request){
+        ExceptionResponse response = ExceptionResponse.of(ExceptionCode.TEST_EXCEPTION, request);
+        return new ResponseEntity<>(response, ExceptionCode.TEST_EXCEPTION.getStatusCode());
     }
 }
